8f30152c13ace35cca6ac6aa28dd526ba5893778
jenniewerner <jenniewerner2018@u.northwestern.edu>
2016-10-30 14:39:43 -0500

    added weather API

---
+++ aacp/NearbyLocations.swift
@@ -0,0 +1,9 @@
+//
+//  NearbyLocations.swift
+//  aacp
+//
+//  Created by Jennie Werner on 10/30/16.
+//  Copyright © 2016 Jennie Werner. All rights reserved.
+//
+
+import Foundation
---
+++ aacp/WeatherAPI.swift
@@ -0,0 +1,9 @@
+W//
+//  File.swift
+//  aacp
+//
+//  Created by Jennie Werner on 10/30/16.
+//  Copyright © 2016 Jennie Werner. All rights reserved.
+//
+
+import Foundation
